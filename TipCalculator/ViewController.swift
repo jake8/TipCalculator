255c9b008ee0df5616bb498c5d6a0a0eee3dbdd2
Jake Stabile <jakes8@me.com>
2015-03-13 20:24:53 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Jake Stabile on 3/13/15.
+//  Copyright (c) 2015 Jake Stabile. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

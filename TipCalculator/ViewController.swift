6e63c03c3385f06e8b0ffe9d6a664d56b33e80c1
Jake Stabile <jakes8@me.com>
2015-03-13 21:27:31 -0400

    initial application

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,10 +10,21 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var totalTextField: NSTextField!
+    @IBOutlet weak var tipSlider: NSSlider!
+    @IBOutlet weak var tipLabel: NSTextField!
+    @IBOutlet weak var calculateButton: NSButton!
+    @IBOutlet weak var tipTotalLabel: NSTextField!
+    @IBOutlet weak var checkTotalLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tipLabel.integerValue = tipSlider.integerValue
+        tipTotalLabel.stringValue = ""
+        checkTotalLabel.stringValue = ""
+        
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +32,19 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    @IBAction func tipSliderValueChanged(sender: AnyObject) {
+        tipLabel.floatValue = round(sender.floatValue)
+    }
+    
+    @IBAction func calculateButtonPressed(sender: AnyObject) {
+        var tip = totalTextField.floatValue * round(tipSlider.floatValue) / 100.00
+        var total = totalTextField.floatValue + tip
+        
+        tipTotalLabel.stringValue = String(format: "$%.02f", tip)
+        checkTotalLabel.stringValue = String(format: "$%.02f", total)
+    }
+    
 
 }
 
